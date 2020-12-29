222466942932b9ba2c3f62168d24b0e8e18c5907
bmd0219 <bmd0219@gmail.com>
2020-12-29 23:41:12 +0530

    [Fix]: Global sockets
    Changed following
    	-implemented socket event listeners of chat and video class globally

--- client/src/components/student/chatComponents/Chat.js
+++ client/src/components/student/chatComponents/Chat.js
@@ -3,14 +3,14 @@ import NavBar from '../mainComponents/NavBar'
 import {Container,Row,Col} from 'react-bootstrap'
 import ChatBox from './ChatBox'
 import Sidebar from '../mainComponents/Sidebar'
-export default function Chat() {
+export default function Chat(props) {
     return (
     <React.Fragment>
         <NavBar/>
         <Container fluid>
         <Row >
             <Col sm={8}className="d-flex mh100 justify-content-center" > 
-                <ChatBox/>     
+                <ChatBox {...props}/>     
             </Col>
             <Col sm={4}className="d-flex justify-content-center" >
                 <Sidebar/>
--- client/src/components/student/onlineclassComponents/JoinRoom.js
+++ client/src/components/student/onlineclassComponents/JoinRoom.js
@@ -1,34 +1,14 @@
-import React,{useEffect,useRef} from 'react'
+import React from 'react'
 import {useSelector,useDispatch} from 'react-redux'
 import {useHistory} from 'react-router-dom'
 import {Button} from 'react-bootstrap'
-import {socket} from "../../../socket";
-import {onlineclasson} from '../../../redux/student/auth/authActions'
 export default function JoinRoom() {
     const auth = useSelector(state => state.studentauth)
     const onlineclass = auth.class.onlineclass;
     const user = useSelector(state => state.user)
     const room = onlineclass.roomno;
-    const classname = auth.class.classname
     const theme = user.theme
     let history = useHistory();
-    const socketRef=useRef()
-    const dispatch = useDispatch()
-    useEffect(() => {
-        socketRef.current = socket;
-        socketRef.current.on('endclass',async (data)=>{
-            if(data.roomID===room){
-                const obj = {roomno:null,profname:null,happening:false}
-                dispatch(onlineclasson(obj))
-            }
-        })
-        socketRef.current.on('callroom',data=>{
-            if(data.classname===classname){
-                const obj = {roomno:data.room,profname:data.profname,happening:true}
-                dispatch(onlineclasson(obj))
-            }
-        })
-    }, [classname,room,dispatch])
     const connect = ()=>{
         history.push(`/watch/${room}`)
     }
